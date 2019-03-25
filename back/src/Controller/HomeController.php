0fc2129617896f6b27c987abf67d7fecd4f7e8cc
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2019-03-25 15:39:25 +0100

    added remote proxy

---
+++ back/src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+}
